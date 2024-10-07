9a125620742f85354734b77e95278ab89e01d7a7
BencsikBoglárBianka <bencsikboglarbianka@ktch.hu>
2024-10-07 23:00:44 +0200

    Rombatll

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+/*
+* File: Rombatl.java
+* Author: Bencsik Boglár Bianka
+* Copyright: 2024, Bencsik Boglár Bianka
+* Group: Szoft II/I/N
+* Date: 2024-10-06
+* Github: https://github.com/BencsikBoglarBianka/Rombatl
+* Licenc: GNU GPL
+*/
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Rombatl rombt = new Rombatl();
+        rombt.RombTerulet();
     }
 }
